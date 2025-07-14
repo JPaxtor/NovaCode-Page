c009f68a78c016da566223b654d43b70f3fcdc22
Manuel Paxtor <kingoffiger@gmail.com>
2025-07-14 17:13:28 -0600

    Se avanza el index y se agrega typed js

---
+++ src/assets/javascript/blog.js
@@ -0,0 +1,14 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#contTyped", {
+  strings: [
+    "Software",
+    "Web Development",
+    "Programming",
+    "Coding",
+    "Tech Enthusiast",
+  ],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true,
+});
